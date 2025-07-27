ce252bd8bff9128f104b77cad19fc088655669d2
Dhron Patel <dhronp110@gmail.com>
2025-07-26 21:39:47 -0500

    ;)

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+// src/components/Header.tsx
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="app-header">
+      <h1 className="logo">TYPER</h1>
+      <p className="slogan">Type faster, innovate smarter</p>
+    </header>
+  );
+};
+
+export default Header;
